7552bf2a3cb08f5fc2f18344fd8b81a4e0a76ce1
Phil Nachum <phil.nachum@airbnb.com>
2016-08-17 22:07:47 -0700

    Add UserProfileViewController

---
+++ TwitterClient/UserProfileViewController.swift
@@ -0,0 +1,35 @@
+//
+//  UserProfileViewController.swift
+//  TwitterClient
+//
+//  Created by phil_nachum on 8/17/16.
+//  Copyright © 2016 phil_nachum. All rights reserved.
+//
+
+import UIKit
+
+class UserProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
